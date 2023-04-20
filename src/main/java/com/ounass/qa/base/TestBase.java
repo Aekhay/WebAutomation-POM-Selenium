8ec6c1f694ebf23088afa039e9f4a59e220f4f7a
wrp <wrp@Aaqibs-MacBook-Pro.local>
2023-04-20 15:59:29 +0500

    added chrome options

--- src/main/java/com/ounass/qa/base/TestBase.java
+++ src/main/java/com/ounass/qa/base/TestBase.java
@@ -50,6 +50,7 @@ public class TestBase {
 			options.addArguments("--disable-gpu");
 			options.addArguments("--disable-extensions");
 			options.addArguments("--dns-prefetch-disable");
+			options.addArguments("--headless");
 			
 		} else if (browserName.equals("FireFox")) {
 			
